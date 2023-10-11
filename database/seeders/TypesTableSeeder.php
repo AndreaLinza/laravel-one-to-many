f8dfd5a72a14f044e0cbe49378bb29e747bd8c26
AndreaLinza <andrealinza@gmail.com>
2023-10-11 21:04:18 +0200

    add seeder

--- database/seeders/TypesTableSeeder.php
+++ database/seeders/TypesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class TypesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            [
+                'name' => 'FrontEnd',
+                'color' => '156,39,176'
+            ],
+            [
+                'name' => 'Backend',
+                'color' => '3,169,244'
+            ],
+            [
+
+                'name' => 'Fullstack',
+                'color' => '255,87,34'
+            ],
+            [
+                'name' => 'Design',
+                'color' => '76,175,80'
+            ]
+        ];
+
+        //faccio un ciclo foreach sull'array che per ogni ciclo 
+        foreach ($types as $type) {
+            //Ci andiamo a creare un nuovo type
+            $new_type = new Type();
+            //assegniamo i valori alle varie proprietà
+            $new_type->name = $type['name'];
+            $new_type->color = $type['color'];
+            //ce lo salviamo
+            $new_type->save();
+        }
     }
 }
